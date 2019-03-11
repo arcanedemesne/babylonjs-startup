ed0a40a292669e2aa5e5b26c50d3265092edece0
Jennifer Allen <arcanedemesne@Jennifers-MacBook-Pro.local>
2019-03-10 20:38:34 -0400

    creating a box

--- src/resources/scene.js
+++ src/resources/scene.js
@@ -1,17 +1,17 @@
 const createScene = ({ canvas, engine }) => {
   // Create the scene space
-  var scene = new BABYLON.Scene(engine);
+  const scene = new BABYLON.Scene(engine);
 
   // Add a camera to the scene and attach it to the canvas
-  var camera = new BABYLON.ArcRotateCamera("Camera", 3 * Math.PI / 4, Math.PI / 4, 4, BABYLON.Vector3.Zero(), scene);
+  const camera = new BABYLON.ArcRotateCamera("Camera", 3 * Math.PI / 4, Math.PI / 4, 4, BABYLON.Vector3.Zero(), scene);
   camera.attachControl(canvas, true);
 
   // Add lights to the scene
-  var light1 = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(1, 1, 0), scene);
-  var light2 = new BABYLON.PointLight("light2", new BABYLON.Vector3(0, 1, -1), scene);
+  const light1 = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(1, 1, 0), scene);
+  const light2 = new BABYLON.PointLight("light2", new BABYLON.Vector3(0, 1, -1), scene);
 
   // Add and manipulate meshes in the scene
-  var box = BABYLON.MeshBuilder.CreateBox("box", {height: .5, width: 0.75, depth: 0.85}, scene);
+  const box = BABYLON.MeshBuilder.CreateBox("box", {height: .5, width: 0.75, depth: 0.85}, scene);
 
   return scene;
 };
